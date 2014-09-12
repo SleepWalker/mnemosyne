8e4a44e7cf07cff9893a1f9366796eb05b6e36fd
SleepWalker <mybox@udf.su>
2014-09-13 01:22:12 +0300

    Moved to my own gulp bootstrap npm package

--- gulp/tasks/fonts.js
+++
@@ -1,6 +0,0 @@
-var gulp = require('gulp');
-
-module.exports = function() {
-    return gulp.src('src/fonts/**')
-    .pipe(gulp.dest('dist/fonts'));
-};
--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,18 @@
-// TODO:
-// переименовать build в src
-//  - gulp-watch
-//  
-// gulp-changed - only pass through changed files
-// gulp-cached - in-memory file cache, not for operation on sets of files
-// gulp-remember - pairs nicely with gulp-cached
-// gulp-newer - pass through newer source files only, supports many:1 source:dest
+var tasks = {
+    'compass': {
+        loadPath: "bower_components/foundation/scss"
+    },
+    'imagemin': {},
+    'fonts': {},
+    'browserify': {},
+    'browsersync': {
+        'host': 'mnemosyne.glp',
+        'injectChanges': true,
+    },
+    'watch': {},
+    'mocha-phantomjs': {},
+    'mocha-browserify': {},
+    };
+var gulp = require('sleepwalker-gulp-bootstrap')(tasks);
 
-
-var tasks = [
-    'compass',
-    'imagemin',
-    'fonts',
-    'browserify',
-    // 'mocha',
-    'mocha-phantomjs',
-    'mocha-browserify',
-    'browsersync'
-    ];
-var gulp = require('./gulp')(tasks);
-
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
-
-// gulp.task('build', ['compass']);
-gulp.task('watch', function() {
-    gulp.watch('src/sass/**', ['compass']);
-    gulp.watch('src/images/**', ['imagemin']);
-    gulp.watch('src/fonts/**', ['fonts']);
-    // gulp.watch(['src/js/**', 'test/**'], ['mocha']);
-    gulp.watch(['src/js/**', 'test/**'], ['mocha-phantomjs']);
-
-    gulp.watch(['../*.{html,php}', 'css/**/*.css', 'js/**/*.js', 'images/**/*.js'], reload);
-});
-gulp.task('default', ['browserify', 'mocha-browserify', 'watch']);
+gulp.task('default', ['browsersync', 'browserify', 'mocha-browserify', 'watch']);
